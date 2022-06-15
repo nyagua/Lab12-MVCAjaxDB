f6e26cae072e9b65046ad3b5c75e4a31ce6eaa63
nyagua <nelson.yagua@tecsup.edu.pe>
2022-06-14 23:00:35 -0500

    Agregar archivos de proyecto.

---
+++ INFRAESTRUCTURE/Migrations/202206150354500_v3.cs
@@ -0,0 +1,20 @@
+﻿namespace INFRAESTRUCTURE.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Students", "studentCode", c => c.String(nullable: false));
+            AddColumn("dbo.Students", "studentLastName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Students", "studentLastName");
+            DropColumn("dbo.Students", "studentCode");
+        }
+    }
+}
---
+++ INFRAESTRUCTURE/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DOMAIN;
+using System.Data.Entity;
+
+namespace INFRAESTRUCTURE
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name = MyContextDB")
+        {
+
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+    }
+}
